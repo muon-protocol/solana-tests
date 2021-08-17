d8d8de9e39f1998d73b32a8d6d26ffe1105aacc2
Zahra Namazi <namazi.q@gmail.com>
2021-08-17 15:11:50 +0430

    add help

---
+++ web3/bridge/help.js
@@ -0,0 +1,9 @@
+console.log(
+  '\t\t',
+  'To Create TOKEN: \n\n \t\t\t',
+  '"npm run create-token"\n \n',
+  '\t\t',
+  'To Mint TOKEN: \n\n \t\t\t',
+  '"npm run mint -- -amount=100"\n \n\t\t\t',
+  '-amount: The amount you want to mint the token \n\n \t\t\t'
+)
