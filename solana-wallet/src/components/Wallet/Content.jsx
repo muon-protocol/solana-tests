b1b125713dbc1edf15c6ac959a545d33010e6d60
Zahra Namazi <namazi.q@gmail.com>
2021-08-26 13:33:14 +0430

    add faucet

--- solana-wallet/src/components/Wallet/Content.jsx
+++ solana-wallet/src/components/Wallet/Content.jsx
@@ -22,6 +22,7 @@ const Content = () => {
   } = useWallet()
   const accountAddress = publicKey?.toString()
   const [balance, setBalance] = React.useState(null)
+  const [isFunded, setIsFunded] = React.useState(false)
   const connection = new Connection('https://api.testnet.solana.com')
   connection
     .getBalance(publicKey)
@@ -41,6 +42,13 @@ const Content = () => {
     disconnecting,
     autoApprove
   })
+  const getSol = async () => {
+    const res = await connection.requestAirdrop(publicKey, 1000000000)
+    if (res) setIsFunded(res)
+    // const balance = await connection.getBalance(publicKey)
+    // console.log({ balance })
+    // setBalance(balance / DECIMAL_OFFSET)
+  }
   return (
     <>
       <div className="row mt-5">
@@ -56,6 +64,14 @@ const Content = () => {
       </div>
       <div className="row col-12 mt-5">
         <h2>Your SOL balances :{balance}</h2>
+      </div>
+      <div className="row col-12 mt-5">
+        <button className="btn btn-success" onClick={getSol}>
+          Faucet
+        </button>
+      </div>
+      <div className="row col-12 mt-5">
+        {isFunded && <div className="alert alert-success">Address Funded!</div>}
       </div>
     </>
   )
